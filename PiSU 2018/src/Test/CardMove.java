7c9ac8658c7b166f7992e692d1ae78606c3527ea
Christian <s175125@student.dtu.dk>
2018-04-08 21:40:25 +0200

    ChanceCard constructor

--- PiSU 2018/src/Test/CardMove.java
+++ PiSU 2018/src/Test/CardMove.java
@@ -1,5 +1,22 @@
 package Test;
-
+/**
+ * Inheriting methods & attributes from the ChanceCard class
+ * @author Christian ++
+ *
+ */
 public class CardMove extends ChanceCard {
 
+	private int test;
+	/**
+	 * Constructor 
+	 * @param cardNumber
+	 * @param text
+	 * @param test
+	 */
+	public CardMove(int cardNumber, String text,int test) {
+		super(cardNumber,text);
+	}
+	
+	
+	
 }
--- PiSU 2018/src/Test/ChanceCard.java
+++ PiSU 2018/src/Test/ChanceCard.java
@@ -1,5 +1,23 @@
 package Test;
 
+/**
+ * Contains methods methods & attributes for the chancecards, which are extended/inherited in
+ * the classes, CardMove, CardPay, CardReceive & CardPrison
+ * @author Christian ++
+ *
+ */
+
+// Bør klassen være abstract?
 public class ChanceCard {
 
+	protected String text;
+	protected int cardNumber;
+	
+	
+	public ChanceCard (int cardNumber, String text) {
+		this.cardNumber = cardNumber;
+		this.text = text;
+	}
+	
+	
 }
