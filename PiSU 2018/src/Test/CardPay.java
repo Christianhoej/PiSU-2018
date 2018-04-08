08cfeab14ada1c7405fe01fc5586cfa69354cf34
Christian <s175125@student.dtu.dk>
2018-04-08 22:24:38 +0200

    Kontruktører i diverse klasser

--- PiSU 2018/src/Test/CardPay.java
+++ PiSU 2018/src/Test/CardPay.java
@@ -1,5 +1,16 @@
 package Test;
-
+/**
+ * Inheriting methods & attributes from the ChanceCard class.
+ * Includes methods for chancecards of when a player must pay money. 
+ * @author Christian ++
+ *
+ */
 public class CardPay extends ChanceCard {
 
+	private int noget;
+	
+	public CardPay(int cardNumber, String text, int noget) {
+		super( cardNumber, text);
+	}
+	
 }
--- PiSU 2018/src/Test/CardPrison.java
+++ PiSU 2018/src/Test/CardPrison.java
@@ -1,5 +1,17 @@
 package Test;
-
+/**
+ * Inheriting methods & attributes from the ChanceCard class
+ * Contains methods for when a player must go to prison, following a drawn chancecard. 
+ * 
+ * @author Christian ++
+ *
+ */
 public class CardPrison extends ChanceCard {
 
+	private int noget;
+	
+	public CardPrison(int cardNumber, String text, int noget) {
+		super(cardNumber, text);
+	}
+	
 }
