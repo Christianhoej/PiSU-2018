f4e2a1b539e9993d15a81467f602f8a3ec0794d3
Christian <s175125@student.dtu.dk>
2018-04-17 12:55:18 +0200

    JDBC

--- PiSU 2018/src/model/Player.java
+++ PiSU 2018/src/model/Player.java
@@ -12,60 +12,59 @@ public class Player {
 	public Player(String name /*, int balance, int position, boolean broke, boolean winner, boolean inPrison*/) {
 		this.name = name;
 		this.account = new Account(30000);
-//		this.position = position;
-//		this.broke = broke;
-//		this.winner = winner;	
-//		this.inPrison = inPrison;
-
+		//		this.position = position;
+		//		this.broke = broke;
+		//		this.winner = winner;	
+		//		this.inPrison = inPrison;
 	}
 	public String getName() {
 		return name;
 	}
-	
+
 	// Smartere at kalde account direkte, istedet for at lave "hjælpe metoder" til at gøre det samme
 	// --> Undgå redundens
 	public Account getAccount() {
 		return account;
 	}
-	
-//	public int[] getAccount1() {
-//		int[] acc = new int[3];
-//		acc[0] = account.getCash();
-//		acc[1] = account.getAssetValue();
-//		acc[2] = account.getPrisonCard();
-//		return acc;
-//	}
-	
-//	public void updateAccount(int cash, int assetValue, int prisonCard) {
-//	account.updateCash(cash);
-//	account.updateAssetValue(assetValue);
-//	account.updatePrisonCard(prisonCard);
-//}
-	
+
+	//	public int[] getAccount1() {
+	//		int[] acc = new int[3];
+	//		acc[0] = account.getCash();
+	//		acc[1] = account.getAssetValue();
+	//		acc[2] = account.getPrisonCard();
+	//		return acc;
+	//	}
+
+	//	public void updateAccount(int cash, int assetValue, int prisonCard) {
+	//	account.updateCash(cash);
+	//	account.updateAssetValue(assetValue);
+	//	account.updatePrisonCard(prisonCard);
+	//}
+
 	public void setPosition(int position) {
 		this.position = position;
 	}
-	
+
 	public int getPosition() {
 		return position;
 	}
-	
+
 	public boolean isBroke() {
 		return broke;
 	}
-	
+
 	public void setBroke(boolean broke) {
 		this.broke = broke;
 	}
-	
+
 	public int getTotalValue() {
 		return account.getCash()+account.getAssetValue();
 	}
-	
+
 	public int getInPrison() {
 		return inPrison;
 	}
-	
+
 	/**
 	 * 
 	 * @param inPrison - 	value, that determines if and how long a player has been in prison.
@@ -79,19 +78,19 @@ public class Player {
 	public void setInPrison(int inPrison) {
 		this.inPrison=inPrison;
 	}
-	
+
 	public void addOwnedProperties(int fieldNumber) {
 		ownedProperties[fieldNumber] = 1;
 	}
-	
+
 	public void removeOwnedProperties(int fieldNumber) {
 		ownedProperties[fieldNumber] = 0;
 	}
-	
+
 	public int[] getOwnedProperties() {
 		return ownedProperties;
 	}
-	
+
 }
 
 //Delvist taget fra CDIO3 - not done - Yoss
