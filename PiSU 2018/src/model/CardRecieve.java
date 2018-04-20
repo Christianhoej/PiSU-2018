f7b9e21cbfed18f4d60437eef9c8c020878b32d8
Y0SSB0SS <josephine.mellin@gmail.com>
2018-04-20 13:29:58 +0200

    Aflæsning fra txt klassen.

--- PiSU 2018/src/model/CardRecieve.java
+++ PiSU 2018/src/model/CardRecieve.java
@@ -1,4 +1,7 @@
 package model;
+
+import model.Txt;
+
 /**
  * Inheriting methods & attributes from the ChanceCard class
  * includes methods for when a player must move his car, following a drawn chancecard. 
@@ -8,10 +11,15 @@ package model;
 
 public class CardRecieve extends ChanceCard {
 
+	public CardRecieve(int cardNumber, String text) {
+		super(cardNumber, text);
+	}
+
+	String[] fieldName = Txt.file("CardRecieve");
 	private int amount;
 	
 	public void receiveMoney(Player player, int amount) {
-		player.getAccount().addCash(amount);
+		player.getAccount().updateCash(amount);
 	}
 
 	public int getAmount() {
