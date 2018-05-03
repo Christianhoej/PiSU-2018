9510d87057ae64ca81102c9f7a01620c9a5a6004
Christian <s175125@student.dtu.dk>
2018-05-03 11:06:35 +0200

    test

--- PiSU 2018/src/test/diceTest.java
+++ PiSU 2018/src/test/diceTest.java
@@ -8,58 +8,45 @@ import model.Dice;
 import java.math.*;
 
 public class diceTest {
+	
 	@Test
 	public void diceTest() {
 
 
-		Dice d = new Dice();
+		Dice d1 = new Dice();
+		Dice d2 = new Dice();
 		Dice diceArray[] = new Dice[10000];
-		int count[] = new int[6];¨
+		int count[] = new int[11];
+		int slag[] = new int[] {2,3,4,5,6,7,8,9,10,11,12};
 		
-		for(int i=0; i<diceArray.length; i++) {
-			diceArray[i]=(Dice)(Math.random()*6+1);
-			
-//			switch (i) {
-//			case 2: count[0]++;
-//			break;
-//			case 3: count[1]++;
-//			break;
-//			case 4: count[2]++;
-//			break;
-//			case 5: count[3]++;
-//			break;
-//			case 6: count[4]++;
-//			break;
-//			case 7: count[5]++;
-//			break;
-//			case 8: count[6]++;
-//			break;
-//			case 9: count[7]++;
-//			break;
-//			case 10: count[8]++;
-//			break;
-//			case 11: count[9]++;
-//			break;
-//			case 12: count[10]++;
-//			break;
-//			}
+		for(int i=0; i<diceArray.length; i++) {			
+			switch(d1.rollDice()+ d2.rollDice()) {
+			case 2: count[1]++;
+			break;
+			case 3: count[2]++;
+			break;
+			case 4: count[3]++;
+			break;
+			case 5: count[4]++;
+			break;
+			case 6: count[5]++;
+			break;
+			case 7: count[6]++;
+			break;
+			case 8: count[7]++;
+			break;
+			case 9: count[8]++;
+			break;
+			case 10: count[9]++;
+			break;
+			case 11: count[10]++;
+			break;
+			case 12: count[11]++;
+			break;
+			}
 		}
-		for(int i=0; i<count.length; i++) {
-			System.out.println("Terningerne har rullet " + count[i]  + diceArray);
-		}
-			
-		
-		
-
-		/**
-		 * Test om den giver den rigtige værdi
-		 */
-//		for(int i = 0;i < 10000;i++){
-//			d.rollDice();
-//			assertTrue(d.getFaceValue() >= 1 && d.getFaceValue() <= 6);
-//			//assertTrue(d.getFaceValue() >= 1 && d.getFaceValue() <= 6);
-//			//assertTrue(db.getDiceSum()>=2 && db.getDiceSum() <=12);
-//		}
-		
+		for(int i=0; i<count.length && i<slag.length; i++) {
+			System.out.println("Terningerne har rullet " + slag[i] + " " + count[i] + " gange");
+		}		
 	}
 }
