5c1c6a1e3a9aee69c835003fc6ad2f97ba8adfeb
Gunn <Gunn@Gunns-PC>
2018-05-04 13:20:41 +0200

    df

--- PiSU 2018/src/model/Fields.java
+++ PiSU 2018/src/model/Fields.java
@@ -1,5 +1,7 @@
 package model;
 
+import java.util.ArrayList;
+
 import controller.GameController;
 import designpatterns.Subject;
 
@@ -46,8 +48,17 @@ public abstract class Fields extends Subject {
 		notifyChange();
 	}
 	
-	public boolean buildHouses(Player player) {
+	public boolean buildHouses(Player player, ArrayList<Fields> fields) {
+		if(player.getOwnedProperties() != null) {
+			for(int i = 0; i<fields.size(); i++) {
+				if(fields.gplayer.getOwnedProperties().get(i))
+			}
+			
+			
+		}
 		
+		
+		return true;
 		
 	}
 	
