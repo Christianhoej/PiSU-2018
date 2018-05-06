3b799d59eaa01e947ac0ae85f4c8bc5a300d1b8f
Christian <s175125@student.dtu.dk>
2018-05-06 22:51:26 +0200

    Test slettet og opdateret

--- PiSU 2018/src/test/AllTests.java
+++ PiSU 2018/src/test/AllTests.java
@@ -5,7 +5,7 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses({ AccountTest.class, diceTest.class, GameControllerTest.class })
+@SuiteClasses({ AccountTest.class, GameControllerTest.class })
 public class AllTests {
 
 }
